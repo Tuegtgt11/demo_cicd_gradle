d0925462b449c414e3d7a6df0fbabc6e00834c98
Vũ Đức Tuệ <vuductue1122@gmail.com>
2023-04-04 14:45:37 +0700

    first update

---
+++ src/main/java/com/example/demo_cicd_gradle/DemoCicdGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_cicd_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCicdGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCicdGradleApplication.class, args);
+	}
+
+}
